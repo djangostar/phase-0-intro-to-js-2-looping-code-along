d8dc83a185611cece1b9eb6aef891ae2053ae544
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2021-12-14 08:39:11 -0800

    commit tree ba0428abb9686e5252c2d152ccb570e1aa9e668e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    let messages= [];
+
+    for(let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+}
+
+function countDown(integer) {
+    
+    while(integer > 0) {
+        console.log(integer)
+        integer -= 1
+    }
+    console.log(integer)
+}

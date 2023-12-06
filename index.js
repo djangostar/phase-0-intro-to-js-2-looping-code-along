74c4220943cb70a1898d9312f008991c5f63bb9e
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2023-12-05 18:25:43 -0800

    Redone

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const writeCards = (arr, events) => {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(`Thank you, ${arr[i]}, for the wonderful ${events} gift!`);
+  }
+  return newArr;
+};
+
+const countDown = (integer) => {
+  while (integer > 0) {
+    console.log(integer);
+    integer -= 1;
+  }
+  console.log(integer);
+};

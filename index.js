7dc61efedf215881384d662b049df737134468c3
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2023-12-21 12:24:59 -0800

    Redone with comments

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 // Code your solutions in this file
-const writeCards = (arr, events) => {
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    newArr.push(`Thank you, ${arr[i]}, for the wonderful ${events} gift!`);
+const writeCards = (arrName, event) => {
+  // create new array of names
+  const newArrName = [];
+  // loop through the original array of names
+  for (let i = 0; i < arrName.length; i++) {
+    // add the names to the new array in each iteration
+    // console.log the message
+    newArrName.push(
+      `Thank you, ${arrName[i]}, for the wonderful ${event} gift!`
+    );
   }
-  return newArr;
+  // return the new array
+  return newArrName;
 };
 
 const countDown = (integer) => {
+  // while the integer is greater than zero, log the integer value
   while (integer > 0) {
     console.log(integer);
+    // minus one through each iteration until zero is reached
     integer -= 1;
   }
+  // console log the integer value
   console.log(integer);
 };
